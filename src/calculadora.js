51240f919ddab9a67319377bd6e49eb8d19fdb43
IsaacMasisV <isaacmv2003@gmail.com>
2024-05-30 15:47:32 -0600

    ejercicio-5

---
+++ src/calculadora.js
@@ -0,0 +1,23 @@
+let calculadora = {
+    
+    sumar:function (num1, num2) {
+       return num1+num2
+    },
+
+    resta:function (num1, num2) {
+        return num1-num2
+     },
+
+
+     divicion:function (num1, num2) {
+        return num1/num2
+     },
+
+
+     multiplicacion:function (num1, num2) {
+        return num1*num2
+     }
+
+}
+
+export {calculadora}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,39 @@
 // Inserte el código aquí
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { calculadora } from "./calculadora";
+
+function calcular (operacion, num1, num2) {
+
+    if (operacion=== "+") {
+         return calculadora.sumar(num1, num2)
+    }
+    if (operacion=== "-") {
+         return calculadora.resta(num1, num2)
+    }
+    
+    if (operacion=== "*") {
+         return calculadora.multiplicacion(num1, num2)
+    }
+    
+    if (operacion=== "/") {
+         return calculadora.divicion(num1, num2)
+    }
+    
+
+}
+console.log(calcular("*",65,85))
