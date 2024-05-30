43a9e3002c65dba49977e7c5369469b5e36bcf4c
IsaacMasisV <isaacmv2003@gmail.com>
2024-05-30 15:50:30 -0600

    ejercicio-4

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 // Inserte el código aquí
 
+// Inserte el código aquí
+
+let h = document.getElementById ('h')
+let m = document.getElementById('m')
+let s = document.getElementById('s')
+let ms = document.getElementById('ms')
+
+setInterval(() => {
+    function reloj  () {
+        let date = new Date ();
+         h.innerHTML = date.getHours();
+         m.innerHTML = date.getMinutes();
+         s.innerHTML = date.getSeconds();
+         ms.innerHTML = date.getse();
+         
+   
+    }
+    reloj()
+    
+},  1000);
+
